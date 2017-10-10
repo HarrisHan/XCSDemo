f1fae3284ae6c409a3c8519b5fcbe76e713bf95d
harris <hanhui@ngs.tech>
2017-10-10 09:16:10 +0800

    first commit

---
+++ XCSDemo/XCSDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCSDemo
+//
+//  Created by Harris on 2017/10/10.
+//  Copyright © 2017年 Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
